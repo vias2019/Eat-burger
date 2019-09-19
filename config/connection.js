d3edf5d69030b5e3e7c866b9fbe4d5ba2fa04223
Victoria Grishkina <vias2010@gmail.com>
2019-09-18 20:16:15 -0700

    Trigger re-deploy

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
